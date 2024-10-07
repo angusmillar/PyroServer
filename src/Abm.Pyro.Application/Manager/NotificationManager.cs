8115621d8863a57f0fc340d3d440981e789a48b6
Angus Millar <angusbmillar@gmail.com>
2024-10-07 19:08:16 +1000

    Improve error handling with try catch and message

--- src/Abm.Pyro.Application/Manager/NotificationManager.cs
+++ src/Abm.Pyro.Application/Manager/NotificationManager.cs
@@ -1,13 +1,23 @@
 ﻿using Abm.Pyro.Application.HostedServiceSupport;
 using Abm.Pyro.Application.Notification;
+using Microsoft.Extensions.Logging;
 
 namespace Abm.Pyro.Application.Manager;
 // ReSharper disable once ClassNeverInstantiated.Global
 public class NotificationManager(
+    ILogger<NotificationManager> logger,
     IRepositoryEventChannel repositoryEventChannel) : ITimedHostedService
 {
     public async Task DoWork(CancellationToken cancellationToken)
     {
-        await repositoryEventChannel.ProcessAsync(cancellationToken);
+        try
+        {
+            await repositoryEventChannel.ProcessAsync(cancellationToken);
+        }
+        catch (Exception e)
+        {
+            logger.LogCritical(e, "Uncaught exception in the IRepositoryEventChannel class");
+            throw;
+        }
     }
 }
