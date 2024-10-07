aa43e2463181dfe9f2df173ba2dce17497cf4e3d
Angus Millar <angusbmillar@gmail.com>
2024-10-07 19:10:36 +1000

    New FhirNotificationService to process repository events and send FHIR Subscription Notifications, still a work in progress

---
+++ src/Abm.Pyro.Application/FhirSubscriptions/FhirNotificationService.cs
@@ -0,0 +1,48 @@
+﻿using Abm.Pyro.Application.Notification;
+using Abm.Pyro.Domain.Enums;
+
+namespace Abm.Pyro.Application.FhirSubscriptions;
+
+public class FhirNotificationService : IFhirNotificationService
+{
+    public async Task ProcessEventList(ICollection<RepositoryEvent> repositoryEventList)
+    {
+        if (repositoryEventList.Count == 0)
+        {
+            return;
+        }
+        
+        //The set of RepositoryEvents in the collection represents all events from a single inbound request on the
+        //FHIR API, they MUST all have the same RequestId
+        ThrowIfInvalidRequestIds(repositoryEventList);
+        
+        var fhirNotifiableEventList = GetFhirNotifiableEventList(repositoryEventList);
+        
+        foreach (RepositoryEvent fhirNotifiableEvent in fhirNotifiableEventList)
+        {
+            await ProcessEvent(fhirNotifiableEvent);
+        }
+        
+    }
+
+    private static void ThrowIfInvalidRequestIds(ICollection<RepositoryEvent> repositoryEventList)
+    {
+        if (!repositoryEventList.All(x => x.RequestId.Equals(repositoryEventList.First().RequestId)))
+        {
+            throw new ApplicationException("All Repository Events in the collection must have the same RequestId");
+        }
+    }
+
+    private static IEnumerable<RepositoryEvent> GetFhirNotifiableEventList(ICollection<RepositoryEvent> repositoryEventList)
+    {
+        RepositoryEventType[] fhirNotifiableEvents = [RepositoryEventType.Create, RepositoryEventType.Update];
+        return repositoryEventList.Where(x => fhirNotifiableEvents.Contains(x.RepositoryEventType));
+    }
+
+    private async Task ProcessEvent(RepositoryEvent repositoryEvent)
+    {
+        //Do the search for the resource adding its ResourceStoreId into the search
+        await Task.Delay(10000);
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Abm.Pyro.Application/FhirSubscriptions/IFhirNotificationService.cs
@@ -0,0 +1,8 @@
+﻿using Abm.Pyro.Application.Notification;
+
+namespace Abm.Pyro.Application.FhirSubscriptions;
+
+public interface IFhirNotificationService
+{
+    Task ProcessEventList(ICollection<RepositoryEvent> repositoryEventList);
+}
